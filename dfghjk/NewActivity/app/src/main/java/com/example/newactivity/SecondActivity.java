d8ccc5e510034ac8fddb07e73de04b20c8f9e73d
camran89 <camranalam89@gmail.com>
2020-03-05 23:31:46 +0530

    initial commit

---
+++ dfghjk/NewActivity/app/src/main/java/com/example/newactivity/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.newactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toast.makeText(SecondActivity.this,"Welcome To the New Activity",Toast.LENGTH_LONG).show();
+    }
+}
